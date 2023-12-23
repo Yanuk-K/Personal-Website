2dbb2df6e544a91eb7ccca70490381a81583887f
Yeunwook Kim <kimyw102699@gmail.com>
2023-12-23 23:54:33 +0900

    first commit

---
+++ personal-website/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Yeunwook Kim</h1>
+      <h3>Software Engineer</h3>
+      <p className="framework-info">Frameworks used to make this website:
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+      </p>
+    </>
+  )
+}
+
+export default App
