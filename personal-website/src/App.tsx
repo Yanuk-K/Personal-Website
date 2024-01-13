00faec53e5c60f616ce523128478ac963b729389
Yeunwook Kim <kimyw102699@gmail.com>
2024-01-14 03:30:09 +0900

    Mistake

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from '/components/Header.tsx'
 
 function App() {
 
   return (
     <>
+      <Header></Header>
       <h1>Yeunwook Kim</h1>
       <h3>Software Engineer</h3>
       <p className="framework-info">Frameworks used to make this website:
