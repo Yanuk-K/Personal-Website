b8b218d0b53a38d277867e7122de6aa6b3d5d180
Yeunwook Kim <kimyw102699@gmail.com>
2025-04-11 23:50:55 +0900

    update location

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -31,11 +31,11 @@ function Contacts() {
           >
             <iframe
               className="h-[100%] w-[100%] border-none"
-              src="https://www.google.com/maps/embed/v1/place?q=Osan+AB+Main+Gate,+Sinjang-ro,+Pyeongtaek-si,+Gyeonggi-do,+South+Korea&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d203317.18639187005!2d127.22209655!3d37.22798575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b523d6ace33f5%3A0x9d3235701951b5fb!2sYongin-si%2C%20Gyeonggi-do!5e0!3m2!1sen!2skr!4v1744382965116!5m2!1sen!2skr"
             ></iframe>
           </div>
         </div>
-        <h3>Current Location: Pyeongtaek-si, Republic of Korea</h3>
+        <h3>Current Location: Yongin-si, Republic of Korea</h3>
         <div className="mr-auto mt-10 flex w-full flex-wrap items-center gap-10">
           {links.map((link, id) => {
             return (
